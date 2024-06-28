ec60e16b620b8742a188f16c392310e4af29b4ab
João Lagos <101952716+JoaoLagos@users.noreply.github.com>
2024-06-27 22:31:52 -0300

    HTML, CSS, images and routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "transacaos#index"
+  # root "transacaos#index"
+
+  authenticated :user do # Se estiver autenticado, entre no root que é "rails/health#show"
+    root to: 'transacaos#index', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in') # Se NÃO estiver autenticado, redirecione para o login
 end
